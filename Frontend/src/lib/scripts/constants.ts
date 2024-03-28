b20def82b352030acfada77c16fc124712a3d904
Liviu`s <58745862+Cartitarul@users.noreply.github.com>
2024-03-28 19:48:08 +0200

    Update constants.ts

--- Frontend/src/lib/scripts/constants.ts
+++ Frontend/src/lib/scripts/constants.ts
@@ -1,5 +1,5 @@
-export const BASE_PATH = import.meta.env.VITE_API_URL || "https://moldovarpg.netlify.app"
-export const INVITE_URL = import.meta.env.VITE_INVITE_URL || "https://moldovarpg.netlify.app"
+export const BASE_PATH = import.meta.env.VITE_API_URL || "https://moldovarpg.com:9990"
+export const INVITE_URL = import.meta.env.VITE_INVITE_URL || "https://moldovarpg.com/discord"
 
 export function get(path: string) {
 
